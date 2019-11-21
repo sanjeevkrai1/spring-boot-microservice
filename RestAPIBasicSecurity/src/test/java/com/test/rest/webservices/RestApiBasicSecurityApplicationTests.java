bcbdcab5a6267a6e4d340dc5ea328e1cd30f3a2e
Sanjeev Rai <sanjeev.rai@u2opiamobile.com>
2019-11-21 06:14:14 +0530

    commit intial for spring security - basic

---
+++ RestAPIBasicSecurity/src/test/java/com/test/rest/webservices/RestApiBasicSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.rest.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiBasicSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
