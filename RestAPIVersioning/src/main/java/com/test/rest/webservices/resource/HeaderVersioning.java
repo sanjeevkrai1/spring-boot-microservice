d41e0c5d1a9dc363fec8f6e4d7b6bc8d25640bc5
Sanjeev Rai <sanjeev.rai@u2opiamobile.com>
2019-11-21 05:41:14 +0530

    just check

--- RestAPIVersioning/src/main/java/com/test/rest/webservices/resource/HeaderVersioning.java
+++ RestAPIVersioning/src/main/java/com/test/rest/webservices/resource/HeaderVersioning.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HeaderVersioning {
-	
 
 	@GetMapping(value = "/person/header" , headers = "X-API-VERSION=1")
 	public Person getPersion() {
